775fa2e1debb72e1deff06e936e31265d943a9fb
Tim Park <timfpark@gmail.com>
2019-01-10 10:16:51 -0800

    Add verbose logging flag

--- cmd/root.go
+++ cmd/root.go
@@ -25,6 +25,12 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	verbose, _ := rootCmd.PersistentFlags().GetBool("verbose")
+
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -34,14 +40,7 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fab.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().Bool("verbose", true, "Verbose output logs")
 }
 
 // initConfig reads in config file and ENV variables if set.
