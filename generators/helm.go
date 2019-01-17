e52df5e674fdad10678fdbf2e880c0a1f955afa4
Tim Park <timfpark@gmail.com>
2019-01-17 06:37:39 -0800

    Switch to local helm installation to avoid init issues with docker image

--- generators/helm.go
+++ generators/helm.go
@@ -64,11 +64,7 @@ func GenerateHelmComponent(component *core.Component) (manifest string, err erro
 	chartPath := path.Join(absHelmRepoPath, component.Path)
 	absCustomValuesPath := path.Join(chartPath, "overriddenValues.yaml")
 
-	log.Debugf("writing config %s to %s\n", configYaml, absCustomValuesPath)
 	ioutil.WriteFile(absCustomValuesPath, configYaml, 0644)
-
-	volumeMount := fmt.Sprintf("%s:/app/chart", chartPath)
-	log.Debugf("templating with volumeMount: %s\n", volumeMount)
 
 	name := component.Name
 	if component.Config.Config["name"] != nil {
@@ -80,7 +76,7 @@ func GenerateHelmComponent(component *core.Component) (manifest string, err erro
 		namespace = component.Config.Config["namespace"].(string)
 	}
 
-	output, err := exec.Command("docker", "run", "--rm", "-v", volumeMount, "alpine/helm:latest", "template", "/app/chart", "--values", "/app/chart/overriddenValues.yaml", "--name", name, "--namespace", namespace).Output()
+	output, err := exec.Command("helm", "template", chartPath, "--values", absCustomValuesPath, "--name", name, "--namespace", namespace).Output()
 
 	if err != nil {
 		if ee, ok := err.(*exec.ExitError); ok {
@@ -120,10 +116,8 @@ func InstallHelmComponent(component *core.Component) (err error) {
 
 	absHelmRepoPath, err := filepath.Abs(helmRepoPath)
 	chartPath := path.Join(absHelmRepoPath, component.Path)
-	volumeMount := fmt.Sprintf("%s:/app/chart", chartPath)
 
 	log.Println(emoji.Sprintf(":helicopter: updating helm chart's dependencies for %s", component.Name))
-	fmt.Println(volumeMount)
 
 	err = exec.Command("helm", "dependency", "update", chartPath).Run()
 
