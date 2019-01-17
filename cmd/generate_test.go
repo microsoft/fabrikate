fc3f4f6f68ba34ac2cdfc405c4358e9b5d143842
Tim Park <timfpark@gmail.com>
2019-01-17 08:05:00 -0800

    Refactor tests to be order invariant and add elasticsearch-curator

--- cmd/generate_test.go
+++ cmd/generate_test.go
@@ -3,42 +3,50 @@ package cmd
 import (
 	"testing"
 
+	"github.com/Microsoft/fabrikate/core"
 	"github.com/stretchr/testify/assert"
 )
+
+func checkComponentLengthsAgainstExpected(t *testing.T, components []core.Component, expectedLengths map[string]int) {
+	for _, component := range components {
+		if expectedLength, ok := expectedLengths[component.Name]; ok {
+			assert.True(t, ok)
+
+			assert.Equal(t, expectedLength, len(component.Manifest))
+		}
+	}
+}
 
 func TestGenerateJSON(t *testing.T) {
 	components, err := Generate("../test/fixtures/generate", "prod")
 
 	assert.Nil(t, err)
 
-	assert.Equal(t, 7, len(components))
+	expectedLengths := map[string]int{
+		"elasticsearch":         14495,
+		"elasticsearch-curator": 2394,
+		"fluentd-elasticsearch": 20203,
+		"kibana":                1595,
+		"static":                188,
+	}
 
-	assert.Equal(t, "static", components[6].Name)
-	assert.Equal(t, 188, len(components[6].Manifest))
+	assert.Equal(t, 8, len(components))
 
-	assert.Equal(t, "elasticsearch", components[3].Name)
-	assert.Equal(t, 13638, len(components[3].Manifest))
-
-	assert.Equal(t, "fluentd-elasticsearch", components[4].Name)
-	assert.Equal(t, 20173, len(components[4].Manifest))
-
-	assert.Equal(t, "kibana", components[5].Name)
-	assert.Equal(t, 1595, len(components[5].Manifest))
+	checkComponentLengthsAgainstExpected(t, components, expectedLengths)
 }
 
 func TestGenerateYAML(t *testing.T) {
 	components, err := Generate("../test/fixtures/generate-yaml", "prod")
 
+	expectedLengths := map[string]int{
+		"prometheus-grafana": 125,
+		"grafana":            8575,
+		"prometheus":         21401,
+	}
+
 	assert.Nil(t, err)
 
 	assert.Equal(t, 3, len(components))
 
-	assert.Equal(t, "prometheus-grafana", components[0].Name)
-	assert.Equal(t, 125, len(components[0].Manifest))
-
-	assert.Equal(t, "grafana", components[1].Name)
-	assert.Equal(t, 8159, len(components[1].Manifest))
-
-	assert.Equal(t, "prometheus", components[2].Name)
-	assert.Equal(t, 21023, len(components[2].Manifest))
+	checkComponentLengthsAgainstExpected(t, components, expectedLengths)
 }
