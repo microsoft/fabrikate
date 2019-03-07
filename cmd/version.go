3d39ba1c312d9d71eac7ae1279943edf21772704
Tim Park <timfpark@gmail.com>
2019-03-07 13:46:10 +0530

    Version 0.2.4 (#79)

--- cmd/version.go
+++ cmd/version.go
@@ -30,7 +30,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("fab version 0.2.3")
+		log.Println("fab version 0.2.4")
 	},
 }
 
