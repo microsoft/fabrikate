4e17839d7f8dcc94003d779f7745f06b0b30fa73
Tim Park <timfpark@gmail.com>
2019-03-19 14:27:36 -0700

    Add set config command

--- core/componentConfig.go
+++ core/componentConfig.go
@@ -1,17 +1,121 @@
 package core
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"path"
+
 	"github.com/timfpark/conjungo"
+	yaml "github.com/timfpark/yaml"
 )
 
 type ComponentConfig struct {
-	Config        map[string]interface{}
-	Subcomponents map[string]ComponentConfig
+	Path          string                     `yaml:"-" json:"-"`
+	Serialization string                     `yaml:"-" json:"-"`
+	Config        map[string]interface{}     `yaml:"config,omitempty" json:"config,omitempty"`
+	Subcomponents map[string]ComponentConfig `yaml:"subcomponents,omitempty" json:"subcomponents,omitempty"`
+}
+
+func NewComponentConfig() ComponentConfig {
+	return ComponentConfig{
+		Config:        map[string]interface{}{},
+		Subcomponents: map[string]ComponentConfig{},
+	}
+}
+
+func (cc *ComponentConfig) GetPath(environment string) string {
+	configFilename := fmt.Sprintf("config/%s.%s", environment, cc.Serialization)
+	return path.Join(cc.Path, configFilename)
+}
+
+func (cc *ComponentConfig) UnmarshalJSONConfig(environment string) (err error) {
+	cc.Serialization = "json"
+	return UnmarshalFile(cc.GetPath(environment), json.Unmarshal, &cc)
+}
+
+func (cc *ComponentConfig) UnmarshalYAMLConfig(environment string) (err error) {
+	cc.Serialization = "yaml"
+	return UnmarshalFile(cc.GetPath(environment), yaml.Unmarshal, &cc)
+}
+
+func (cc *ComponentConfig) MergeConfigFile(environment string) (err error) {
+	componentConfig := NewComponentConfig()
+
+	if err := componentConfig.Load(environment); err != nil {
+		return err
+	}
+
+	return cc.Merge(componentConfig)
+}
+
+func (cc *ComponentConfig) Load(environment string) (err error) {
+	err = cc.UnmarshalYAMLConfig(environment)
+
+	// fall back to looking for JSON if loading YAML fails.
+	if err != nil {
+		err = cc.UnmarshalJSONConfig(environment)
+
+		if err != nil {
+			// couldn't find any config files, so default back to yaml serialization
+			cc.Serialization = "yaml"
+		}
+	}
+
+	return nil
+}
+
+func (cc *ComponentConfig) SetComponentConfig(path []string, value string) {
+	configLevel := cc.Config
+
+	for levelIndex, pathPart := range path {
+		// if this key is not the final one, we need to decend in the config.
+		if levelIndex < len(path)-1 {
+			if _, ok := configLevel[pathPart]; !ok {
+				configLevel[pathPart] = map[string]interface{}{}
+			}
+
+			configLevel = configLevel[pathPart].(map[string]interface{})
+		} else {
+			configLevel[pathPart] = value
+		}
+	}
+}
+
+func (cc *ComponentConfig) SetConfig(subcomponentPath []string, path []string, value string) {
+	subcomponentConfig := *cc
+	for _, subcomponentName := range subcomponentPath {
+		if subcomponentConfig.Subcomponents == nil {
+			subcomponentConfig.Subcomponents = map[string]ComponentConfig{}
+		}
+
+		if _, ok := subcomponentConfig.Subcomponents[subcomponentName]; !ok {
+			subcomponentConfig.Subcomponents[subcomponentName] = NewComponentConfig()
+		}
+
+		subcomponentConfig = subcomponentConfig.Subcomponents[subcomponentName]
+	}
+
+	subcomponentConfig.SetComponentConfig(path, value)
 }
 
 func (cc *ComponentConfig) Merge(newConfig ComponentConfig) (err error) {
 	options := conjungo.NewOptions()
 	options.Overwrite = false
 
-	return conjungo.Merge(cc, newConfig, options)
+	err = conjungo.Merge(cc, newConfig, options)
+
+	return err
+}
+
+func (cc *ComponentConfig) Write(environment string) (err error) {
+	var marshaledConfig []byte
+
+	if cc.Serialization == "json" {
+		marshaledConfig, err = json.MarshalIndent(cc, "", "  ")
+	} else {
+		marshaledConfig, err = yaml.Marshal(cc)
+	}
+
+	return ioutil.WriteFile(cc.GetPath(environment), marshaledConfig, 0644)
 }
--- core/componentConfig_test.go
+++ core/componentConfig_test.go
@@ -1,57 +1,40 @@
 package core
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMergeConfig(t *testing.T) {
-	currentConfig := ComponentConfig{
-		Config: map[string]interface{}{},
-		Subcomponents: map[string]ComponentConfig{
-			"jaeger": ComponentConfig{
-				Config: map[string]interface{}{
-					"provisionDataStore": map[string]interface{}{
-						"elasticsearch": false,
-						"mixed":         1,
-						"slice": []interface{}{
-							"astring",
-							1,
-							1.0,
-							map[string]interface{}{
-								"foo": 1,
-								"bar": []interface{}{
-									"bstring",
-									1,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
+func TestLoad(t *testing.T) {
+	config := ComponentConfig{
+		Path: "../test/fixtures/load",
 	}
 
-	newConfig := ComponentConfig{
-		Config: map[string]interface{}{},
-		Subcomponents: map[string]ComponentConfig{
-			"jaeger": ComponentConfig{
-				Config: map[string]interface{}{
-					"provisionDataStore": map[string]interface{}{
-						"cassandra":     false,
-						"elasticsearch": true,
-						"mixed":         "2",
-					},
-				},
-			},
-		},
-	}
+	err := config.Load("test")
+	assert.Nil(t, err)
 
-	err := currentConfig.Merge(newConfig)
+	assert.Equal(t, "bar", config.Config["foo"])
+}
+
+func TestMerge(t *testing.T) {
+	currentConfig := NewComponentConfig()
+	currentConfig.Path = "../test/fixtures/merge"
+	err := currentConfig.Load("current")
+	assert.Nil(t, err)
+
+	newConfig := NewComponentConfig()
+	newConfig.Path = "../test/fixtures/merge"
+	err = newConfig.Load("new")
+	assert.Nil(t, err)
+
+	err = currentConfig.Merge(newConfig)
 	assert.Nil(t, err)
 
 	jaegerSubcomponent := currentConfig.Subcomponents["jaeger"]
+
 	provisionConfig := jaegerSubcomponent.Config["provisionDataStore"].(map[string]interface{})
 
 	cassandraValue := provisionConfig["cassandra"].(bool)
@@ -65,4 +48,81 @@ func TestMergeConfig(t *testing.T) {
 
 	sliceValues := provisionConfig["slice"].([]interface{})
 	assert.Equal(t, 1, sliceValues[1].(int))
+}
+
+func TestSet(t *testing.T) {
+	config := NewComponentConfig()
+	config.Path = "../test/fixtures/load"
+
+	err := config.Load("test")
+	assert.Nil(t, err)
+
+	// can override a config value successfully
+	config.SetConfig([]string{}, []string{"foo"}, "fee")
+	assert.Equal(t, "fee", config.Config["foo"])
+
+	// can create a value successfully
+	config.SetConfig([]string{}, []string{"new"}, "value")
+	assert.Equal(t, "value", config.Config["new"])
+
+	// can override a subcomponent value successfully
+	config.SetConfig([]string{"myapp"}, []string{"zoo"}, "zee")
+	assert.Equal(t, "zee", config.Subcomponents["myapp"].Config["zoo"])
+
+	// can create a new deeper subcomponent config level successfully
+	config.SetConfig([]string{"myapp"}, []string{"data", "storageClass"}, "fast")
+	dataMap := config.Subcomponents["myapp"].Config["data"].(map[string]interface{})
+	assert.Equal(t, "fast", dataMap["storageClass"])
+}
+
+func TestWriteYAML(t *testing.T) {
+	err := os.Remove("../test/fixtures/write/config/test.yaml")
+
+	config := ComponentConfig{
+		Path:          "../test/fixtures/write",
+		Serialization: "yaml",
+		Config: map[string]interface{}{
+			"foo": "bar",
+		},
+		Subcomponents: map[string]ComponentConfig{
+			"myapp": ComponentConfig{
+				Config: map[string]interface{}{
+					"zoo": "zar",
+				},
+			},
+		},
+	}
+
+	err = config.Write("test")
+	assert.Nil(t, err)
+
+	configContents, err := ioutil.ReadFile("../test/fixtures/write/config/test.yaml")
+	assert.Nil(t, err)
+	assert.Equal(t, 70, len(configContents))
+}
+
+func TestWriteJSON(t *testing.T) {
+	err := os.Remove("../test/fixtures/write/config/test.json")
+
+	config := ComponentConfig{
+		Path:          "../test/fixtures/write",
+		Serialization: "json",
+		Config: map[string]interface{}{
+			"foo": "bar",
+		},
+		Subcomponents: map[string]ComponentConfig{
+			"myapp": ComponentConfig{
+				Config: map[string]interface{}{
+					"zoo": "zar",
+				},
+			},
+		},
+	}
+
+	err = config.Write("test")
+	assert.Nil(t, err)
+
+	configContents, err := ioutil.ReadFile("../test/fixtures/write/config/test.json")
+	assert.Nil(t, err)
+	assert.Equal(t, 132, len(configContents))
 }
