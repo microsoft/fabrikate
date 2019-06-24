db977e200606daab7f9d81400e2329c3fc54c603
Evan Louie <evan.louie@microsoft.com>
2019-06-24 05:05:16 -0700

    bump to v0.12.0 (#202)

--- cmd/version.go
+++ cmd/version.go
@@ -30,7 +30,7 @@ var versionCmd = &cobra.Command{
 
 // PrintVersion prints the current version of Fabrikate being used.
 func PrintVersion() {
-	log.Println("fab version 0.11.0")
+	log.Println("fab version 0.12.0")
 }
 
 func init() {
