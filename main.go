45ab958c46cef54437b5905f620144fe037fb75e
Evan Louie <evan.louie@microsoft.com>
2019-07-03 11:08:35 -0700

    Fix race condition for YAML unmashalling (#211)
    - Fixes a race condition for yaml unmarshalling by moving the modification of go-yaml DefaultMapType to very start of program; no longer changing it every time an unmarshal occurs.

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"reflect"
+
 	"github.com/microsoft/fabrikate/cmd"
 	log "github.com/sirupsen/logrus"
+	"github.com/timfpark/yaml"
 )
 
 func main() {
+	// modify the DefaultMapType of yaml to map[string]interface{} instead of map[interface]interface{}
+	*yaml.DefaultMapType = reflect.TypeOf(map[string]interface{}{})
+
 	formatter := new(log.TextFormatter)
 	formatter.TimestampFormat = "02-01-2006 15:04:05"
 	formatter.FullTimestamp = true
