b2b7ec2438abacf8607d1d460b5aaeac2d12d310
Marcel Dias <marceldiass@gmail.com>
2020-02-20 00:12:44 -0300

    Fix identation

--- core/componentConfig.go
+++ core/componentConfig.go
@@ -20,7 +20,7 @@ type ComponentConfig struct {
 	Serialization   string                     `yaml:"-" json:"-"`
 	Namespace       string                     `yaml:"namespace,omitempty" json:"namespace,omitempty"`
 	InjectNamespace bool                       `yaml:"injectNamespace,omitempty" json:"injectNamespace,omitempty"`
-	Disabled 		bool                       `yaml:"disabled,omitempty" json:"disabled,omitempty"`
+	Disabled        bool                       `yaml:"disabled,omitempty" json:"disabled,omitempty"`
 	Config          map[string]interface{}     `yaml:"config,omitempty" json:"config,omitempty"`
 	Subcomponents   map[string]ComponentConfig `yaml:"subcomponents,omitempty" json:"subcomponents,omitempty"`
 }
